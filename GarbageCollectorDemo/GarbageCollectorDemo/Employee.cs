06ae303aa82e77150821eb99ba52d974fc3dbbfc
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-12-28 18:13:46 +0530

    Push New Project

---
+++ GarbageCollectorDemo/GarbageCollectorDemo/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollectorDemo
+{
+    internal class Employee:IDisposable
+    {
+        public int EmpCode { get; set; }
+
+        public string  EmpName { get; set; }
+        public Employee()
+        {
+
+        }
+
+        //  Destructor also called as Findalize method.
+        ~Employee()
+        {
+            GC.Collect();
+            Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            Debug.WriteLine("Destructor Invoked");
+        }
+
+        public void Dispose()
+        {
+           GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ GarbageCollectorDemo/GarbageCollectorDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollectorDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // To create quickly disposable object
+            using (Employee employee = new Employee { EmpCode=100,EmpName="Scott"})
+            {
+                Console.WriteLine($"Code={employee.EmpCode}\t Name={employee.EmpName}");
+            }
+
+           
+                Console.ReadLine();
+        }
+    }
+}
